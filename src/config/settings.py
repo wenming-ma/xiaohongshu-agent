d2680136c8d6e4886b244241b0650ffbe2100f1a
wemingma <wemingma@gmail.com>
2026-04-01 01:08:27 +0800

    Switch default ASR provider back to faster_whisper to avoid Qwen CUDA crash

--- src/config/settings.py
+++ src/config/settings.py
@@ -192,7 +192,7 @@ class ReferenceImageConfig:
 
 
 class ASRConfig:
-    PROVIDER = os.getenv("ASR_PROVIDER", "qwen")
+    PROVIDER = os.getenv("ASR_PROVIDER", "faster_whisper")
 
 
 class SubtitleConfig:
